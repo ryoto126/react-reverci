ae224136a37e459436a591213b2bfafa71d1e13a
ryoto126 <snlingzinn@gmail.com>
2021-06-29 00:49:45 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,119 +1,201 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { isHeritageClause } from 'typescript';
 import './index.css';
 
+const BOARD_SIZE = 8;
+const dx = [1, 1, 0, -1, -1, -1, 0, 1], dy = [0, 1, 1, 1, 0, -1, -1, -1]; //周囲8マス
+//石の色
+const color = {
+    false: "black",
+    true: "white",
+    null: "green"
+};
+
+//マス
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    )
+            <div class="circle" style={{ background: color[props.value] }} />
+        </button >
+    );
 }
 
+//盤面
 class Board extends React.Component {
-
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                {/* <div className="status">{status}</div> */}
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
+    //初期盤面
     constructor(props) {
         super(props);
         this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-            }],
-            xIsNext: true,
-            stepNumber: 0,
+            squares: Array(BOARD_SIZE).fill().map(() => Array(BOARD_SIZE).fill(null)),
+            player: false,
         };
+        this.state.squares[BOARD_SIZE / 2 - 1][BOARD_SIZE / 2 - 1] = this.state.squares[BOARD_SIZE / 2][BOARD_SIZE / 2] = true;
+        this.state.squares[BOARD_SIZE / 2 - 1][BOARD_SIZE / 2] = this.state.squares[BOARD_SIZE / 2][BOARD_SIZE / 2 - 1] = false;
     }
 
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) return;
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares,
-            }]),
-            xIsNext: !this.state.xIsNext,
-            stepNumber: history.length,
-        });
+    //マス(i,j)をクリックしたときの処理
+    handleClick(i, j) {
+        //終了状態でなく、おけるマスがない場合はパス
+        if (!this.isTerminal() && !this.canMove(this.state.player)) this.pass();
+        //終了状態 または、選択したマスに置けない場合は何もせずreturn
+        if (this.isTerminal() || !this.canPut(i, j, this.state.player)) {
+            return;
+        }
+        //マス(i,j)に石をおいて手番を進める
+        this.put(i, j);
     }
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0,
-        })
+    //マス(i,j)の描画
+    renderSquare(i, j) {
+        return (
+            <Square
+                value={this.state.squares[i][j]}
+                onClick={() => this.handleClick(i, j)}
+            />
+        );
     }
-
-    render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-
-        const moves = history.map((step, move) => {
-            const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
+    //ゲームの状態(次の手番、石の数、勝利者)を取得
+    getStatus() {
+        let next_player = 'Next player: ' + (this.state.player ? "Second" : "First");
+        let count = this.countDisks();
+        let first_count = 'First: ' + count[0];
+        let second_count = 'Second: ' + count[1];
+        if (this.isTerminal()) {
+            let winner;
+            if (count[0] > count[1]) winner = "Winner: First";
+            else if (count[0] < count[1]) winner = "Winner: Second";
+            else winner = "Draw";
             return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            )
-        })
-
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
+                <div className="status">
+                    {winner}<br></br>
+                    {first_count}<br></br>
+                    {second_count}
+                </div>
+            );
         }
         else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            return (
+                <div className="status">
+                    {next_player} <br></br>
+                    {first_count}<br></br>
+                    {second_count}
+                </div>
+            );
         }
+
+    }
+
+    //Square要素を8×8個生成して描画
+    render() {
+        return (
+            <div>
+                {this.getStatus()}
+                {
+                    Array(BOARD_SIZE).fill(null).map((val, i) => {
+                        return (
+                            <div className="board-row">
+                                {
+                                    Array(BOARD_SIZE).fill(null).map((val, j) => {
+                                        return this.renderSquare(i, j);
+                                    })
+                                }
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+    //マス(i,j)が盤面の内部に存在するか
+    isInside(i, j) {
+        return i >= 0 && i < BOARD_SIZE && j >= 0 && j <= BOARD_SIZE;
+    }
+    //playerが石を置けるマスが存在するか
+    canMove(player) {
+        for (let i = 0; i < BOARD_SIZE; i++)for (let j = 0; j < BOARD_SIZE; j++) {
+            if (this.canPut(i, j, player)) return true;
+        }
+        return false;
+    }
+    //playerがマス(i,j)に石をおけるか
+    canPut(i, j, player) {
+        const squares = this.state.squares.slice();
+        if (squares[i][j] != null) return false; //マス(i,j)は空きマスでなければならない
+        for (let dir = 0; dir < 8; dir++) {
+            let nx = i + dx[dir];
+            let ny = j + dy[dir];
+            if (!this.isInside(nx, ny)) continue;
+            if (squares[nx][ny] != (!player)) continue; //隣接したマスに相手の石が置かれていなければならない
+            for (let k = 0; k < BOARD_SIZE; k++) {
+                nx += dx[dir];
+                ny += dy[dir];
+                if (!this.isInside(nx, ny)) break;
+                if (squares[nx][ny] == null) break;
+                if (squares[nx][ny] == player) return true;
+            }
+        }
+        return false;
+    }
+    //終了状態の判定
+    isTerminal() {
+        return !this.canMove(false) && !this.canMove(true);
+    }
+    //(i,j)に石を置く
+    put(i, j) {
+        let squares = this.state.squares.slice();
+        let player = this.state.player;
+        squares[i][j] = player;
+        for (let dir = 0; dir < 8; dir++) {
+            let nx = i + dx[dir], ny = j + dy[dir];
+            if (!this.isInside(nx, ny)) continue;
+            if (squares[nx][ny] != (!player)) continue;
+            for (let k = 0; k < BOARD_SIZE; k++) {
+                nx += dx[dir];
+                ny += dy[dir];
+                if (!this.isInside(nx, ny)) break;     //はみ出たらアウト
+                if (squares[nx][ny] == '.') break;  //空きマスでもアウト
+
+                // i,jからnx,nyまで全部ひっくり返す
+                if (squares[nx][ny] == player) {
+                    let x = i + dx[dir], y = j + dy[dir];
+                    while (x != nx || y != ny) {
+                        squares[x][y] = player;  //ひっくり返す
+                        x += dx[dir];
+                        y += dy[dir];
+                    }
+                    break;
+                }
+            }
+        }
+        this.setState({
+            squares: squares,
+            player: !this.state.player,
+        });
+    }
+    //手番を渡す処理
+    pass() {
+        this.setState({
+            player: !this.state.player,
+        });
+    }
+    //石の個数をカウント
+    countDisks() {
+        let ret = [0, 0];
+        for (let i = 0; i < BOARD_SIZE; i++)for (let j = 0; j < BOARD_SIZE; j++) {
+            if (this.state.squares[i][j] == true) ret[1] += 1;
+            else if (this.state.squares[i][j] == false) ret[0] += 1;
+        }
+        return ret;
+    }
+
+}
+
+class Game extends React.Component {
+    render() {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board
-                        squares={current.squares}
-                        onClick={(i) => this.handleClick(i)}
-                    />
-                </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
+                    <Board />
                 </div>
             </div>
         );
@@ -126,24 +208,3 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
-
-
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
-}
